1a0cd8751102a3be589380f1943c7e918a0050fc
Derrick Walker <dwalker39@gmail.com>
2014-10-08 16:28:45 -0700

    Initial commit

--- DWBubbleMenuButtonExample/ViewController.m
+++ DWBubbleMenuButtonExample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "DWBubbleMenuButton.h"
 
 @interface ViewController ()
 
@@ -14,14 +15,93 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)viewDidAppear:(BOOL)animated {
+    // Create down menu button
+    UILabel *homeLabel = [self createHomeButtonView];
+    
+    DWBubbleMenuButton *downMenuView = [[DWBubbleMenuButton alloc] initWithFrame:CGRectMake(20.f,
+                                                                                        20.f,
+                                                                                        homeLabel.frame.size.width,
+                                                                                        homeLabel.frame.size.height)
+                                                          expansionDirection:DirectionDown];
+    downMenuView.homeButtonView = homeLabel;
+    
+    [downMenuView addButtons:[self createDemoButtonArray]];
+    
+    [self.view addSubview:downMenuView];
+    
+    // Create up menu button
+    homeLabel = [self createHomeButtonView];
+    
+    DWBubbleMenuButton *upMenuView = [[DWBubbleMenuButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width - homeLabel.frame.size.width - 20.f,
+                                                                                      self.view.frame.size.height - homeLabel.frame.size.height - 20.f,
+                                                                                      homeLabel.frame.size.width,
+                                                                                      homeLabel.frame.size.height)
+                                                        expansionDirection:DirectionUp];
+    upMenuView.homeButtonView = homeLabel;
+    
+    [upMenuView addButtons:[self createDemoButtonArray]];
+    
+    [self.view addSubview:upMenuView];
+}
+
+- (UILabel *)createHomeButtonView {
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0.f, 0.f, 40.f, 40.f)];
+    
+    label.text = @"Tap";
+    label.textColor = [UIColor whiteColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.layer.cornerRadius = label.frame.size.height / 2.f;
+    label.backgroundColor =[UIColor colorWithRed:0.f green:0.f blue:0.f alpha:0.5f];
+    label.clipsToBounds = YES;
+    
+    return label;
+}
+
+- (NSArray *)createDemoButtonArray {
+    NSMutableArray *buttonsMutable = [[NSMutableArray alloc] init];
+    
+    int i = 0;
+    for (NSString *title in @[@"A", @"B", @"C", @"D", @"E", @"F"]) {
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+        
+        [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [button setTitle:title forState:UIControlStateNormal];
+        
+        button.frame = CGRectMake(0.f, 0.f, 30.f, 30.f);
+        button.layer.cornerRadius = button.frame.size.height / 2.f;
+        button.backgroundColor = [UIColor colorWithRed:0.f green:0.f blue:0.f alpha:0.5f];
+        button.clipsToBounds = YES;
+        button.tag = i++;
+        
+        [button addTarget:self action:@selector(test:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [buttonsMutable addObject:button];
+    }
+    
+    return [buttonsMutable copy];
+}
+
+- (void)test:(UIButton *)sender {
+    NSLog(@"Button tapped, tag: %ld", (long)sender.tag);
+}
+
+- (UIButton *)createButtonWithName:(NSString *)imageName {
+    UIButton *button = [[UIButton alloc] init];
+    [button setImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
+    [button sizeToFit];
+    
+    [button addTarget:self action:@selector(test:) forControlEvents:UIControlEventTouchUpInside];
+    
+    return button;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return true;
 }
 
 @end
---
+++ Source/DWBubbleMenuButton.h
@@ -0,0 +1,56 @@
+//
+//  DWBubbleMenuButton.h
+//  DWBubbleMenuButtonExample
+//
+//  Created by Derrick Walker on 10/8/14.
+//  Copyright (c) 2014 Derrick Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSUInteger, ExpansionDirection) {
+    DirectionLeft = 0,
+    DirectionRight,
+    DirectionUp,
+    DirectionDown
+};
+
+
+@class DWBubbleMenuButton;
+
+@protocol DWBubbleMenuViewDelegate <NSObject>
+
+@optional
+- (void)willExpand:(DWBubbleMenuButton *)expandableView;
+- (void)didExpand:(DWBubbleMenuButton *)expandableView;
+- (void)willCollapse:(DWBubbleMenuButton *)expandableView;
+- (void)didCollapse:(DWBubbleMenuButton *)expandableView;
+
+@end
+
+@interface DWBubbleMenuButton : UIView
+
+@property (nonatomic, weak, readonly) NSArray *buttons;
+@property (nonatomic, readonly) BOOL isCollapsed;
+@property (nonatomic, strong) UIView *homeButtonView;
+
+@property (nonatomic) enum ExpansionDirection direction;
+@property (nonatomic) BOOL animatedHighlighting;
+@property (nonatomic) BOOL collapseAfterSelection;
+@property (nonatomic) float animationDuration;
+@property (nonatomic) float standbyAlpha;
+@property (nonatomic) float highlightAlpha;
+@property (nonatomic) float buttonSpacing;
+
+@property (nonatomic, weak) id <DWBubbleMenuViewDelegate> delegate;
+
+// Initializers
+- (id)initWithFrame:(CGRect)frame expansionDirection:(ExpansionDirection)direction;
+
+// Public Methods
+- (void)addButtons:(NSArray *)buttons;
+- (void)addButton:(UIButton *)button;
+- (void)showButtons;
+- (void)dismissButtons;
+
+@end
